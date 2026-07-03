c3230c7efe0cd87916f7327e3c463e832bdb31fc
Juan Pablo Zegarra Chávez <88498104+JuanPabloZCH@users.noreply.github.com>
2026-07-03 08:18:26 -0500

    Sugerir Herramienta

--- components/footer.js
+++ components/footer.js
@@ -42,7 +42,7 @@ const JPZCH_FOOTER = `
         <ul class="space-y-3">
           <li><a href="/acerca/" class="text-[#64748B] hover:text-[#CCFF00] text-sm transition-colors duration-300">Acerca de</a></li>
           <li><a href="/reportar-error/" class="text-[#64748B] hover:text-[#CCFF00] text-sm transition-colors duration-300">Reportar Error</a></li>
-          <li><a href="#" class="text-[#64748B] hover:text-[#CCFF00] text-sm transition-colors duration-300">Sugerir Herramienta</a></li>
+          <li><a href="/sugerir-herramienta/" class="text-[#64748B] hover:text-[#CCFF00] text-sm transition-colors duration-300">Sugerir Herramienta</a></li>
           <li><a href="#" class="text-[#64748B] hover:text-[#CCFF00] text-sm transition-colors duration-300">Privacidad</a></li>
         </ul>
       </div>
